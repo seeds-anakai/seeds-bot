acbc7ea82dc01f182125c59e7d72c42c1659ae28
seeds-anakai <a.nakai@seeds-std.co.jp>
2023-12-27 15:06:30 +0900

    add reference modal

--- srv/app.Api.ts
+++ srv/app.Api.ts
@@ -19,7 +19,8 @@ import {
 import {
   App,
   AwsLambdaReceiver,
-  SayFn,
+  DividerBlock,
+  SectionBlock,
 } from '@slack/bolt';
 
 // Slack - Web API
@@ -53,19 +54,18 @@ const receiver = new AwsLambdaReceiver({
 const app = new App({ token: slackBotToken, receiver });
 
 // Slack - App Mention Handler
-app.event('app_mention', async ({ client, event, say }) => {
+app.event('app_mention', async ({ client, event }) => {
   await answer(
     client,
     event.channel,
     event.ts,
     event.thread_ts ?? event.ts,
     normalize(event.text),
-    say,
   );
 });
 
 // Slack - Message Handler
-app.event('message', async ({ client, event, say }) => {
+app.message(async ({ client, event }) => {
   if (!event.subtype && event.text) {
     await answer(
       client,
@@ -73,13 +73,53 @@ app.event('message', async ({ client, event, say }) => {
       event.ts,
       event.thread_ts ?? event.ts,
       normalize(event.text),
-      say,
     );
   }
 });
 
+// Slack - Open Reference Action Handler
+app.action('open_reference', async ({ client, body, ack }) => {
+  await ack();
+
+  if (body.type === 'block_actions' && body.actions[0].type === 'button') {
+    // Reference
+    const { content, location } = JSON.parse(body.actions[0].value);
+
+    // Location URI
+    const uri = location?.s3Location?.uri?.split?.('/')?.pop?.() ?? '';
+
+    // Open reference modal.
+    await client.views.open({
+      trigger_id: body.trigger_id,
+      view: {
+        type: 'modal',
+        title: {
+          type: 'plain_text',
+          text: uri.length > 24 ? `${uri.substring(0, 23)}…` : uri,
+        },
+        blocks: [
+          {
+            type: 'header',
+            text: {
+              type: 'plain_text',
+              text: uri,
+            },
+          },
+          {
+            type: 'section',
+            text: {
+              type: 'plain_text',
+              text: content?.text ?? '',
+            },
+          },
+        ],
+      },
+    });
+  }
+});
+
 // Answer
-const answer = async (client: WebClient, channel: string, ts: string, threadTs: string, text: string, say: SayFn): Promise<void> => {
+const answer = async (client: WebClient, channel: string, ts: string, threadTs: string, text: string): Promise<void> => {
   // Thread ID
   const threadId = `${channel}#${threadTs}`;
 
@@ -91,7 +131,7 @@ const answer = async (client: WebClient, channel: string, ts: string, threadTs: 
   });
 
   // Retrieve and generate.
-  const { sessionId, output } = await bedrock.send(new RetrieveAndGenerateCommand({
+  const { sessionId, citations } = await bedrock.send(new RetrieveAndGenerateCommand({
     sessionId: await getSessionId(threadId),
     input: {
       text,
@@ -105,8 +145,71 @@ const answer = async (client: WebClient, channel: string, ts: string, threadTs: 
     },
   }));
 
-  // Say answer in thread.
-  await say({ text: output?.text, thread_ts: threadTs });
+  // Answer Blocks
+  const blocks: [SectionBlock, DividerBlock, ...SectionBlock[]] = [
+    {
+      type: 'section',
+      text: {
+        type: 'plain_text',
+        text: '',
+      },
+    },
+    {
+      type: 'divider',
+    },
+  ];
+
+  // Add answer blocks.
+  citations?.forEach?.(({ generatedResponsePart, retrievedReferences }) => {
+    Object.assign(blocks[0], {
+      text: {
+        type: 'plain_text',
+        text: `${blocks[0].text?.text}${generatedResponsePart?.textResponsePart?.text}`,
+      },
+    });
+
+    // Add references.
+    retrievedReferences?.forEach?.(({ content, location }) => {
+      // Reference No
+      const referenceNo = blocks.length - 1;
+
+      Object.assign(blocks[0], {
+        text: {
+          type: 'plain_text',
+          text: `${blocks[0].text?.text}[${referenceNo}] `,
+        },
+      });
+
+      blocks.push({
+        type: 'section',
+        text: {
+          type: 'plain_text',
+          text: `[${referenceNo}] ${location?.s3Location?.uri?.split?.('/')?.pop?.()}`,
+        },
+        accessory: {
+          type: 'button',
+          text: {
+            type: 'plain_text',
+            text: '詳細を見る',
+          },
+          value: JSON.stringify({
+            content: {
+              text: content?.text?.substring?.(0, 1932 - JSON.stringify(location?.s3Location?.uri).length - (JSON.stringify(content.text).length - content.text.length)),
+            },
+            location,
+          }),
+          action_id: 'open_reference',
+        },
+      });
+    });
+  });
+
+  // Post answer in thread.
+  await client.chat.postMessage({
+    channel,
+    blocks,
+    thread_ts: threadTs,
+  });
 
   // Save session id.
   await dynamodb.send(new PutCommand({
@@ -143,8 +246,17 @@ const getSessionId = async (threadId: string): Promise<string | undefined> => {
 };
 
 // Lambda Handler
-export const handler = async (event: any, context: any, callback: any): Promise<any> => {
-  if (!event.headers['x-slack-retry-num']) {
-    return await receiver.start().then((handler) => handler(event, context, callback));
+export const handler: ReturnType<typeof receiver.toHandler> = async (event, context, callback) => {
+  if (event.headers['x-slack-retry-num']) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'OK',
+      }),
+    };
   }
+
+  return await receiver.start().then((handler) => {
+    return handler(event, context, callback);
+  });
 };
